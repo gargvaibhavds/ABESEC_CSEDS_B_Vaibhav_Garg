33f6b57e2265a42da461647fe4cd2db783d8db0c
Garg <garg@Gargs-MacBook-Pro.local>
2025-09-16 07:34:47 +0530

    dsa

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
+
+        // primitve datatype
+        // int rollno = 64;
+        // char letter = 'r';
+        // float marks = 98.67f;
+        // double largedecimalnumbers = 456754.5431;
+        // long largeInteger = 34464562343464245L;
+        // boolean check = false;
+
+
     }
 }
